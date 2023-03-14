d8f9a486fb26b316ff4d8cc4856d01ee978c120b
guilhermemen <gmendes.sam@gmail.com>
2023-03-14 14:50:23 -0300

    Update 01Calculadora.jsx

--- introducao/src/components/01Calculadora.jsx
+++ introducao/src/components/01Calculadora.jsx
@@ -12,12 +12,15 @@ const Calculadora = () => {
 
     const dividir = (x,y) => x/y
 
+    const renderizar = () => <h1>JSX função interna</h1>
+
     return (
         <div>
             <h1>O resultado da soma é: {somar(20,4)} </h1>
             <h1>O resultado da soma é: {subtrair(20,4)} </h1>
             <h1>O resultado da soma é: {multiplicar(20,4)} </h1>
             <h1>O resultado da soma é: {dividir(20,4)} </h1>
+            {renderizar()}
         </div> 
     )
 }
