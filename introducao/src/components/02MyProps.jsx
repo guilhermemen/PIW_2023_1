f1a0dfa4e7fc1b34e9f613351771d6942ade10ef
guilhermemen <gmendes.sam@gmail.com>
2023-03-20 21:10:19 -0300

    Create 02MyProps.jsx

---
+++ introducao/src/components/02MyProps.jsx
@@ -0,0 +1,9 @@
+const MyProps = (props) => {
+    return (
+        <div>
+            <h1>nome é: {props.nome} e meu curso é: {props.curso}</h1>     
+        </div>
+    )
+}
+
+export default MyProps
