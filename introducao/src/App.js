c31f2790bd5556f28d362d9b7210d86d98a6b722
guilhermemen <gmendes.sam@gmail.com>
2023-03-20 21:10:25 -0300

    Update App.js

--- introducao/src/App.js
+++ introducao/src/App.js
@@ -1,11 +1,18 @@
 // import Helloworld from './components/00helloworld';
-import Calculadora from './components/01Calculadora';
+// import Calculadora from './components/01Calculadora';
+// import MyProps from './components/02MyProps';
+
+import MeusDados from './Atividade/Atividade00/02MeusDados';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Calculadora/>
+
+      <MeusDados nome="Luigi"/>
+      {/* <MyProps nome="Mário" curso="sdknslnf"/> */}
+      {/* <MeusDados/> */}
     </div>
   );
 }
