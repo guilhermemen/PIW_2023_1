4cdcc870daa9bcd9371db0933d272712a51908d1
guilhermemen <gmendes.sam@gmail.com>
2023-03-20 21:10:28 -0300

    Create 01MeusDados.jsx

---
+++ introducao/src/Atividade/Atividade00/01MeusDados.jsx
@@ -0,0 +1,32 @@
+//V0
+// const MeusDados = () => {
+//     return (
+//         <div>
+//             <h1>Antonio Guilherme Mnedes Sanpaio</h1>
+//             <h1>kaschalkdhfvoiajdeoalk</h1>
+//         </div>
+//     )
+// }
+
+//V1    
+// function MeusDados(){
+//     return (
+//         <div>
+//             <h1>Antonio Guilherme Mendws Sampaio</h1>
+//         </div>
+//     )
+// }
+
+//V2
+// import React from "react"
+// class MeusDados extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h1>mendes mendes mdends mendes</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// export default MeusDados
