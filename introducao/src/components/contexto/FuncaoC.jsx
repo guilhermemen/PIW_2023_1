b4b5f26bc4c709a8fd3c179fc9c988e67a639873
guilhermemen <gmendes.sam@gmail.com>
2023-05-14 13:19:29 -0300

    Create FuncaoC.jsx

---
+++ introducao/src/components/contexto/FuncaoC.jsx
@@ -0,0 +1,12 @@
+import FuncaoD from "./FuncaoD"
+
+const FuncaoC = () =>{
+    return(
+        <div>
+            <h1>funcaoC</h1>
+            <FuncaoD/>
+        </div>
+    )
+}
+
+export default FuncaoC
