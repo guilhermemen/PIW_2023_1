4c8c97fefa5bb11115f2e55ce699d7017f32d8e0
guilhermemen <gmendes.sam@gmail.com>
2023-05-14 13:20:30 -0300

    Update 03Temperatura.jsx

--- introducao/src/Atividade/Atividade00/03Temperatura.jsx
+++ introducao/src/Atividade/Atividade00/03Temperatura.jsx
@@ -1,17 +1,26 @@
 const Temperatura = () => {
-    const kelvin = (k) =>{
-        let c = k * 9
-        let f = k * 10
+
+    const celsiusParaFarenheit = (celsius) => (9*celsius+160)/5
+    const farenheitParaCelsius = (farenheit) => (5*farenheit-160)/5
+
+    const kelvin = (kelvin) =>{
+        let celsius = kelvin - 273;
+        let farenheit = 9 * kelvin - 297
         
-        return {c, f}
+        return {celsius, farenheit}
     }
 
-    let {celsius, farenheit} = kevin(100)
-    
+    let c = 30
+    let f = 54
+    let k = 0
+
+    let {celsius, farenheit} = kelvin(k)
+
     return (
         <div>
-            <h1>kelvin para celsius: {celsius}</h1>
-            <h1>kelvin para celsius: {farenheit}</h1>
+            <h1>A temperatura {c} em Celsius é em farenheit: {celsiusParaFarenheit(c)} </h1>
+            <h1>A temperatura {f} em farenheit é em celsius: {farenheitParaCelsius(f)} </h1>
+            <h1>A temperatura em kelvin é: {kelvin(k).celsius} </h1>
         </div>
     )
 }
