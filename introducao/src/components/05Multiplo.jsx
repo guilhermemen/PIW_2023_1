7a90f93046c31e44b3e781a342e8dbae9438f2f7
guilhermemen <gmendes.sam@gmail.com>
2023-05-14 13:19:41 -0300

    Create 05Multiplo.jsx

---
+++ introducao/src/components/05Multiplo.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+    return(
+        <div>
+            <h1>cabeçalho</h1>
+        </div>
+    )
+}
+
+const Body = () => {
+    return(
+        <div>
+            <h1>corpo</h1>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return(
+        <div>
+            <h1>rodapé</h1>
+        </div>
+    )
+}
+
+export {Header, Body, Footer}
