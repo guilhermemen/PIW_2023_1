5135f86168da858d66d636778741c34dcc7cd9a8
guilhermemen <74994168+guilhermemen@users.noreply.github.com>
2023-05-15 15:23:13 -0300

    Update Questao01X.jsx

--- AP01/primeira/src/Questão01/Questao01X.jsx
+++ AP01/primeira/src/Questão01/Questao01X.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import Questao01Y from './Questao01Y';
+// import React, { useState } from 'react';
+// import Questao01Y from './Questao01Y';
 
-function Questao01X() {
+// function Questao01X() {
 
-  const alunos = [
-    { nome: "Sicrano", notas: { ap1: 8.4, ap2: 5.4 } },
-    { nome: "Beltrano", notas: { ap1: 6.7, ap2: 3.5 } },
-    { nome: "Fulano", notas: { ap1: 7.3, ap2: 9.2 } }
-  ];
+//   const alunos = [
+//     { nome: "Sicrano", notas: { ap1: 8.4, ap2: 5.4 } },
+//     { nome: "Beltrano", notas: { ap1: 6.7, ap2: 3.5 } },
+//     { nome: "Fulano", notas: { ap1: 7.3, ap2: 9.2 } }
+//   ];
 
-  const [medias, setMedias] = useState([]);
+//   const [medias, setMedias] = useState([]);
 
-  const mediaFinal = (medias) => {
-    setMedias(medias);
-  };
+//   const handleMediaChange = (medias) => {
+//     setMedias(medias);
+//   };
 
-  return (
-    <div>
-      <Questao01Y alunos={alunos} onMediaChange={mediaFinal} />
+//   return (
+//     <div>
+//       <Questao01Y alunos={alunos} onMediaChange={handleMediaChange} />
 
-      <h2>Alunos com média menor ou igual a 5.0:</h2>
-      <ul>
-        {alunos.map((aluno, index) => (
-          medias[index] <= 5.0 && <li key={index}>{aluno.nome}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//       <h2>Alunos com média menor ou igual a 5.0:</h2>
+//       <ul>
+//         {alunos.map((aluno, index) => (
+//           medias[index] <= 5.0 && <li key={index}>{aluno.nome}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default Questao01X;
+// export default Questao01X;
