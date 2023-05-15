458e19972dfb388bbd17276d98474a4db593c4ca
guilhermemen <74994168+guilhermemen@users.noreply.github.com>
2023-05-15 15:01:19 -0300

    Update Questao01Y.jsx

--- AP01/primeira/src/Questão01/Questao01Y.jsx
+++ AP01/primeira/src/Questão01/Questao01Y.jsx
@@ -1,12 +1,3 @@
 import React, { useEffect } from 'react';
 
-const Questao01Y = ({ alunos, onMediaChange }) => {
-    useEffect(() => {
-      const medias = alunos.map(aluno => (aluno.notas.ap1 + aluno.notas.ap2) / 2);
-      onMediaChange(medias);
-    }, [alunos, onMediaChange]);
-  
-    return null; // Este componente não renderiza nada
-  };
-
 export default Questao01Y
