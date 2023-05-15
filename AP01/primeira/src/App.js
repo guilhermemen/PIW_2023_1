df56ee96cee29d9a1b185655374ea9b2d4284351
guilhermemen <74994168+guilhermemen@users.noreply.github.com>
2023-05-15 14:14:12 -0300

    Create App.js

---
+++ AP01/primeira/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Questao02 from './Questão02/Questao02';
+import Questa01X from './Questão01/Questao01X';
+import Questa01Y from './Questão01/Questao01Y';
+
+function App() {
+  return (
+    <div className="App">
+      <Questao02/>
+      <Questa01X/>
+      <Questa01Y/>
+    </div>
+  );
+}
+
+export default App;
