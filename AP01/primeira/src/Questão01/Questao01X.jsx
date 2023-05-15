2d5a9e4b3fdb845d18125f7e19e05fd289d483e1
guilhermemen <74994168+guilhermemen@users.noreply.github.com>
2023-05-15 14:52:06 -0300

    Update Questao01X.jsx

--- AP01/primeira/src/Questão01/Questao01X.jsx
+++ AP01/primeira/src/Questão01/Questao01X.jsx
@@ -1,5 +1,32 @@
-function Questao01X(){
+import React, { useState } from 'react';
+import Questao01Y from './Questao01Y';
 
+function Questao01X() {
+
+  const alunos = [
+    { nome: "Sicrano", notas: { ap1: 8.4, ap2: 5.4 } },
+    { nome: "Beltrano", notas: { ap1: 6.7, ap2: 3.5 } },
+    { nome: "Fulano", notas: { ap1: 7.3, ap2: 9.2 } }
+  ];
+
+  const [medias, setMedias] = useState([]);
+
+  const handleMediaChange = (medias) => {
+    setMedias(medias);
+  };
+
+  return (
+    <div>
+      <Questao01Y alunos={alunos} onMediaChange={handleMediaChange} />
+
+      <h2>Alunos com média menor ou igual a 5.0:</h2>
+      <ul>
+        {alunos.map((aluno, index) => (
+          medias[index] <= 5.0 && <li key={index}>{aluno.nome}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Questao01X
+export default Questao01X;
