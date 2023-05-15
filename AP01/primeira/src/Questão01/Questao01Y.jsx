7c8420ba0c147d612c568979a850975b7d06f0a2
guilhermemen <74994168+guilhermemen@users.noreply.github.com>
2023-05-15 14:14:33 -0300

    Create Questao01Y.jsx

---
+++ AP01/primeira/src/Questão01/Questao01Y.jsx
@@ -0,0 +1,12 @@
+import react, { useState} from "react";
+
+const Questao01Y = () =>{
+
+    const alunos = [
+        { nome: "Sicrano", notas: {ap1: 8.4, ap2: 5.4} },
+        { nome: "Beltrano", notas: {ap1: 6.7, ap2: 3.5} },
+        { nome: "Fulano", notas: {ap1: 7.3, ap2: 9.2} }
+        ]
+}
+
+export default Questao01Y
