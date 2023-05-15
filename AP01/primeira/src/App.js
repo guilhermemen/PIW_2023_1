6840c218b9b56854da313fd3567a1e738935e643
guilhermemen <74994168+guilhermemen@users.noreply.github.com>
2023-05-15 15:23:05 -0300

    Update App.js

--- AP01/primeira/src/App.js
+++ AP01/primeira/src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import Questao02 from './Questão02/Questao02';
-import Questa01X from './Questão01/Questao01X';
-import Questa01Y from './Questão01/Questao01Y';
+import Questao03 from './Questão03/Questão03';
+// import Questa01X from './Questão01/Questao01X';
+// import Questa01Y from './Questão01/Questão01Y';
 
 function App() {
   return (
     <div className="App">
       <Questao02/>
-      <Questa01X/>
-      <Questa01Y/>
+      {/* <Questa01X/>
+      <Questa01Y/> */}
+      <Questao03/>
     </div>
   );
 }
